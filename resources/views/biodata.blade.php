7529f0d54d63aec5aa6d27fdce2d59059b8bb705
rezakurniawann <reza.kurniawan0110@gmail.com>
2024-08-28 17:22:34 +0700

    first commit

---
+++ resources/views/biodata.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>BIODATA</h1>
+    <table>
+        <tr>
+            <td>Nama</td>
+            <td>: Reza Kurniawan</td>
+        </tr>
+        <tr>
+            <td>Tempat/Tgl Lahir</td>
+            <td>: Tangerang, 17-08-2004</td>
+        </tr>
+        <tr>
+            <td>Jenis kelamin</td>
+            <td>: Laki-laki</td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>: Islam</td>
+        </tr>
+        <tr>
+            <td>Status Perkawinan</td>
+            <td>: Belum Kawin</td>
+        </tr>
+        <tr>
+            <td>Pekerjaan</td>
+            <td>: Pelajar/Mahasiswa</td>
+        </tr>
+        <tr>
+            <td>Kewarganegaraan</td>
+            <td>: WNI</td>
+        </tr>
+    </table>
+</body>
+</html>
